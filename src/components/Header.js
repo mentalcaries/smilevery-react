24078faa831207731d2fac136485c1ff24f18b3f
Devin Jaggernauth <neexis@gmail.com>
2021-10-08 10:00:09 -0400

    put static map

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import headerLogo from '../images/logo.png'
+import gift from '../images/gift.png'
+import love from '../images/love.png'
 import {Link} from 'react-router-dom'
 
 function Header(){
   return(
+    <div>
     <Link to="/smilevery-react/">
     <img className="header__logo" alt="Smilevery Logo" src={headerLogo}/>
     </Link>
+     
+    </div>
+
 
   )
 }
--- src/components/Location.js
+++ src/components/Location.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Map from './utils/Map';
-// import interativeMap from '../images/Map.png'
-
+import interativeMap from '../images/Map.png';
+import {Link} from 'react-router-dom';
 
 function Location() {
   return (
     <div>
       <h2 className="location__title">Location</h2>
       <p className="location__text">Where are you sending this Smilevery?</p>
-      {/* <img className="location__map" src={interativeMap}/> */}
-    <Map/>
-      
+      <Link to="/smilevery-react/shop">
+        <img className="location__map" src={interativeMap} />
+      </Link>
+      {/* <Map/> */}
     </div>
   );
 }
